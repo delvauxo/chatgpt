2dfde926861a1458c6bb0efa958c145ad57195b7
delvauxo <delvauxo@outlook.com>
2023-03-21 18:40:00 +0100

    chatBot can now remember what is said earlier in the conversation but there still is a bug while too many requests...

--- controllers/chatgpt-controller.js
+++ controllers/chatgpt-controller.js
@@ -18,27 +18,41 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+// Declare an array to store the messages
+let messages = [];
+
+// Modify the chatCompletion function to add the user's message to the array
 const chatCompletion = async (req, res) => {
     try {
         if (oliver.dislike) {
             oliver.dislike += 'n\aime pas ';
         }
 
+        // Add the user's message to the array
+        messages.push({ role: "user", content: req.body.text });
+
         const completion = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [
                 { "role": "system", "content": "You are a helpful assistant that always answers in French." },
                 { "role": "system", "content": "Always before answering, take into consideration these User's information : " + props + " !" },
-                { role: "user", content: req.body.text }
+                // Use the messages array to pass in all messages, including the user's message
+                ...messages,
             ],
         });
+
         const message = completion.data.choices[0].message;
+
+        // Add the chatBot's message to the array
+        messages.push(message);
+
         res.status(200).json({ message });
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "Something went wrong." });
     }
 };
+
 
 module.exports = { chatCompletion };
 

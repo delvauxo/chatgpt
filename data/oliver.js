c8fa784c9f6dcdfa21b792f954d2a32db70f9e34
delvauxo <delvauxo@outlook.com>
2023-03-21 19:20:42 +0100

    bubbles alignments

--- data/oliver.js
+++ data/oliver.js
@@ -7,7 +7,7 @@ const Oliver = {
     weightGoal: 'objectif poids : 70kg',
     tall: '1m87',
     country: 'Belgium',
-    personality: 'introverti, gentil, calme, réfléchi, altruiste, ouvert d\'esprit',
+    personality: 'calme, réfléchi, altruiste, ouvert d\'esprit, bienveillant',
     dislike: 'n\aime pas choux de bruxelles, n\aime pas pamplemousse',
     likeALot: 'adore chicken, adore peanuts butter, adore rice, adore sauces, adore RnB, adore amis, adore sorties, adore voyager, adore activités'
 };
